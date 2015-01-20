ad7ad809c8aa74e769f8e094a598b79a8fc4bffa
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-01-20 18:36:14 +0300

    JS backend: add cli test for creating metadata and ant test for library with metadata

--- compiler/tests/org/jetbrains/kotlin/cli/KotlincExecutableTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/cli/KotlincExecutableTestGenerated.java
@@ -165,6 +165,12 @@ public class KotlincExecutableTestGenerated extends AbstractKotlincExecutableTes
             JetTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/cli/js"), Pattern.compile("^(.+)\\.args$"), false);
         }
 
+        @TestMetadata("createMetadata.args")
+        public void testCreateMetadata() throws Exception {
+            String fileName = JetTestUtils.navigationMetadata("compiler/testData/cli/js/createMetadata.args");
+            doJsTest(fileName);
+        }
+
         @TestMetadata("jsExtraHelp.args")
         public void testJsExtraHelp() throws Exception {
             String fileName = JetTestUtils.navigationMetadata("compiler/testData/cli/js/jsExtraHelp.args");
