f554ec8f25e368b0217d697bad203752fd5d4eee
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-01-27 01:06:02 +0300

    JsAnalyzerFacade: support for metadata

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/JsAnalyzerFacade.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/JsAnalyzerFacade.kt
@@ -16,23 +16,33 @@
 
 package org.jetbrains.kotlin.idea.caches.resolve
 
-import org.jetbrains.kotlin.resolve.lazy.ResolveSession
-import org.jetbrains.kotlin.analyzer.ResolverForModule
-import org.jetbrains.kotlin.analyzer.PlatformAnalysisParameters
-import org.jetbrains.kotlin.analyzer.AnalyzerFacade
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.context.GlobalContext
-import org.jetbrains.kotlin.analyzer.ResolverForProject
-import org.jetbrains.kotlin.js.analyze.TopDownAnalyzerFacadeForJS
-import org.jetbrains.kotlin.platform.PlatformToKotlinClassMap
-import org.jetbrains.kotlin.resolve.lazy.declarations.DeclarationProviderFactoryService
-import org.jetbrains.kotlin.resolve.BindingTraceContext
-import org.jetbrains.kotlin.di.InjectorForLazyResolve
-import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
-import org.jetbrains.kotlin.analyzer.ModuleInfo
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileVisitor
+import com.intellij.util.PathUtil
+import org.jetbrains.kotlin.analyzer.AnalyzerFacade
 import org.jetbrains.kotlin.analyzer.ModuleContent
+import org.jetbrains.kotlin.analyzer.ModuleInfo
+import org.jetbrains.kotlin.analyzer.PlatformAnalysisParameters
+import org.jetbrains.kotlin.analyzer.ResolverForModule
+import org.jetbrains.kotlin.analyzer.ResolverForProject
+import org.jetbrains.kotlin.context.GlobalContext
+import org.jetbrains.kotlin.descriptors.impl.CompositePackageFragmentProvider
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.descriptors.PackageFragmentProvider
+import org.jetbrains.kotlin.di.InjectorForLazyResolve
+import org.jetbrains.kotlin.js.analyze.TopDownAnalyzerFacadeForJS
 import org.jetbrains.kotlin.js.resolve.KotlinJsCheckerProvider
+import org.jetbrains.kotlin.platform.PlatformToKotlinClassMap
+import org.jetbrains.kotlin.resolve.BindingTraceContext
+import org.jetbrains.kotlin.resolve.lazy.declarations.DeclarationProviderFactoryService
+import org.jetbrains.kotlin.resolve.lazy.ResolveSession
+import org.jetbrains.kotlin.serialization.js.*
 import org.jetbrains.kotlin.types.DynamicTypesAllowed
+import org.jetbrains.kotlin.utils.LibraryUtils
 
 public class JsResolverForModule(
         override val lazyResolveSession: ResolveSession
@@ -58,7 +68,24 @@ public object JsAnalyzerFacade : AnalyzerFacade<JsResolverForModule, PlatformAna
         val injector = InjectorForLazyResolve(project, globalContext, moduleDescriptor, declarationProviderFactory, BindingTraceContext(),
                                               KotlinJsCheckerProvider, DynamicTypesAllowed())
         val resolveSession = injector.getResolveSession()!!
-        moduleDescriptor.initialize(resolveSession.getPackageFragmentProvider())
+        var packageFragmentProvider = resolveSession.getPackageFragmentProvider()
+
+        if (moduleInfo is LibraryInfo) {
+            val files = moduleInfo.library.getFiles(OrderRootType.CLASSES)
+            val metadataList: MutableList<LibraryUtils.Metadata> = arrayListOf()
+            for(file in files) {
+                metadataList.addAll(LibraryUtils.loadMetadataFromLibrary(PathUtil.getLocalPath(file)!!))
+            }
+            if (metadataList.size() > 0) {
+                val providers = arrayListOf<PackageFragmentProvider>()
+                providers.add(packageFragmentProvider)
+                for (metadata in metadataList) {
+                    providers.addAll(KotlinJavascriptSerializationUtil.getPackageFragmentProviders(moduleDescriptor, metadata.body))
+                }
+                packageFragmentProvider = CompositePackageFragmentProvider(providers)
+            }
+        }
+        moduleDescriptor.initialize(packageFragmentProvider)
         return JsResolverForModule(resolveSession)
     }
 
