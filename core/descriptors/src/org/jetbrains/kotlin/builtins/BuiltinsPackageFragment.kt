8d2917215886711ddf9632ad8268b661fdc03bfa
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-02-17 04:48:19 +0300

    add BuiltinBasePackageFragment

--- core/descriptors/src/org/jetbrains/kotlin/builtins/BuiltinsPackageFragment.kt
+++ core/descriptors/src/org/jetbrains/kotlin/builtins/BuiltinsPackageFragment.kt
@@ -16,89 +16,15 @@
 
 package org.jetbrains.kotlin.builtins
 
-import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedPackageMemberScope
-import org.jetbrains.kotlin.descriptors.ModuleDescriptor
-import org.jetbrains.kotlin.descriptors.PackageFragmentProvider
-import org.jetbrains.kotlin.descriptors.PackageFragmentProviderImpl
-import org.jetbrains.kotlin.descriptors.impl.PackageFragmentDescriptorImpl
-import org.jetbrains.kotlin.name.*
-import org.jetbrains.kotlin.storage.StorageManager
 import java.io.InputStream
-import org.jetbrains.kotlin.serialization.deserialization.DeserializationComponents
-import com.google.protobuf.ExtensionRegistryLite
-import org.jetbrains.kotlin.serialization.deserialization.ClassDataFinder
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.serialization.deserialization.FlexibleTypeCapabilitiesDeserializer
-import org.jetbrains.kotlin.serialization.builtins.BuiltInsProtoBuf
-import org.jetbrains.kotlin.serialization.NameSerializationUtil
-import org.jetbrains.kotlin.serialization.ProtoBuf
-import org.jetbrains.kotlin.serialization.ClassData
+import org.jetbrains.kotlin.storage.StorageManager
 
 public class BuiltinsPackageFragment(
         fqName: FqName,
         storageManager: StorageManager,
         module: ModuleDescriptor,
-        private val loadResource: (path: String) -> InputStream?
-) : PackageFragmentDescriptorImpl(module, fqName) {
-
-    private val extensionRegistry: ExtensionRegistryLite
-
-    ;{
-        extensionRegistry = ExtensionRegistryLite.newInstance()
-        BuiltInsProtoBuf.registerAllExtensions(extensionRegistry)
-        extensionRegistry
-    }
-
-    private val nameResolver = NameSerializationUtil.deserializeNameResolver(
-            getStream(BuiltInsSerializationUtil.getStringTableFilePath(fqName))
-    )
-
-    public val provider: PackageFragmentProvider = PackageFragmentProviderImpl(listOf(this))
-
-    private val members: DeserializedPackageMemberScope = run {
-        val proto = loadPackage()
-        DeserializedPackageMemberScope(
-                this,
-                proto,
-                nameResolver,
-                DeserializationComponents(
-                        storageManager, module, BuiltInsClassDataFinder(),
-                        BuiltInsAnnotationAndConstantLoader(getContainingDeclaration()),
-                        provider, FlexibleTypeCapabilitiesDeserializer.ThrowException
-                ),
-                { readClassNames(proto) }
-        )
-    }
-
-    private fun loadPackage(): ProtoBuf.Package {
-        val stream = getStream(BuiltInsSerializationUtil.getPackageFilePath(fqName))
-        return ProtoBuf.Package.parseFrom(stream, extensionRegistry)
-    }
-
-    private fun readClassNames(proto: ProtoBuf.Package): List<Name> {
-        return proto.getExtension(BuiltInsProtoBuf.className)?.map { id -> nameResolver.getName(id) } ?: listOf()
-    }
-
-    override fun getMemberScope() = members
-
-    private fun getStream(path: String): InputStream =
-            loadResource(path) ?: throw IllegalStateException("Resource not found in classpath: $path")
-
-    private inner class BuiltInsClassDataFinder : ClassDataFinder {
-        override fun findClassData(classId: ClassId): ClassData? {
-            val metadataPath = BuiltInsSerializationUtil.getClassMetadataPath(classId) ?: return null
-            val stream = loadResource(metadataPath) ?: return null
-
-            val classProto = ProtoBuf.Class.parseFrom(stream, extensionRegistry)
-
-            val expectedShortName = classId.getRelativeClassName().shortName()
-            val actualShortName = nameResolver.getClassId(classProto.getFqName()).getRelativeClassName().shortName()
-            if (!actualShortName.isSpecial() && actualShortName != expectedShortName) {
-                // Workaround for case-insensitive file systems,
-                // otherwise we'd find "Collection" for "collection" etc
-                return null
-            }
-
-            return ClassData(nameResolver, classProto)
-        }
-    }
-}
+        loadResource: (path: String) -> InputStream?
+) : BuiltinsBasePackageFragment(fqName, storageManager, module, FlexibleTypeCapabilitiesDeserializer.ThrowException, loadResource)
