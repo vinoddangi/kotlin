7b8e7b320521fd62cc7078aca80c52f7a24a2b44
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2015-02-19 14:52:09 +0300

    ~test update

--- compiler/testData/codegen/boxInline/smap/anonymousLambda.1.kt
+++ compiler/testData/codegen/boxInline/smap/anonymousLambda.1.kt
@@ -16,3 +16,31 @@ fun box(): String {
     return test()
 }
 
+
+//SMAP
+//anonymousLambda.1.kt
+//Kotlin
+//*S Kotlin
+//*F
+//+ 1 anonymousLambda.1.kt
+//_DefaultPackage
+//+ 2 anonymousLambda.2.kt
+//builders/BuildersPackage
+//*L
+//1#1,46:1
+//4#2:47
+//*E
+//
+//SMAP
+//anonymousLambda.2.kt
+//Kotlin
+//*S Kotlin
+//*F
+//+ 1 anonymousLambda.2.kt
+//builders/BuildersPackage$anonymousLambda_2$HASH$call$1
+//+ 2 anonymousLambda.1.kt
+//_DefaultPackage$anonymousLambda_1$HASH
+//*L
+//1#1,18:1
+//8#2:19
+//*E
--- compiler/testData/codegen/boxInline/smap/anonymousObject.1.kt
+++ compiler/testData/codegen/boxInline/smap/anonymousObject.1.kt
@@ -15,3 +15,31 @@ fun test(): String {
 fun box(): String {
     return test()
 }
+
+//SMAP
+//anonymousObject.1.kt
+//Kotlin
+//*S Kotlin
+//*F
+//+ 1 anonymousObject.1.kt
+//_DefaultPackage
+//+ 2 anonymousObject.2.kt
+//builders/BuildersPackage
+//*L
+//1#1,45:1
+//4#2,5:46
+//*E
+//
+//SMAP
+//anonymousObject.2.kt
+//Kotlin
+//*S Kotlin
+//*F
+//+ 1 anonymousObject.2.kt
+//builders/BuildersPackage$anonymousObject_2$HASH$call$1
+//+ 2 anonymousObject.1.kt
+//_DefaultPackage$anonymousObject_1$HASH
+//*L
+//1#1,21:1
+//8#2:22
+//*E
